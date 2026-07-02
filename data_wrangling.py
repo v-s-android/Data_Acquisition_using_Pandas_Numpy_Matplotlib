41a3776fb1f3c0482225eddc7fa227c944555d36
Vineel Sadineni <vin.anddev@gmail.com>
2026-07-02 14:52:55 -0700

    Add mean calculation and NaN replacement for columns
    Calculate mean values for 'normalized-losses' and 'bore' columns, replacing NaN values with the calculated averages.

--- data_wrangling.py
+++ data_wrangling.py
@@ -66,9 +66,41 @@ the number of "True" values.
 for column in missing_data_df.columns.values.tolist():
   print(missing_data_df[column].value_counts())
   print("")
-        
 
+'''
+output:
+symboling
+False    205
+Name: count, dtype: int64
 
+normalized-losses
+False    164
+True      41
+Name: count, dtype: int64
+'''
+
+# Calculate the mean value for the "normalized-losses" column
+avg_norm_losses = df["normalized-losses"].astype("float").mean(axis = 0)
+print("Average/Mean of normalized-losses:",avg_norm_losses) # output: Average of normalized-losses: 122.0
+
+#Replace NaN in "normalized-losses" column with avg_norm_losses
+df["normalized-losses"].replace(np.nan , avg_norm_losses , inplace = True) # Alt: df["normalized-losses"] = df["normalized-losses"].replace(np.nan, avg_norm_loss)
+
+#Calculate the mean valure if "bore" column and replace NaN with avg_bore_val
+avg_bore_val = df["bore"].astype("float").mean(axis = 0)
+print("Average of bore: ", avg_bore_val) # Average of bore: 3.3297512437810943
+df["bore"] = df["bore"].replace( np.nan ,avg_bore_val) 
+print(df["bore"],head(5))
+
+'''
+output: 
+0    3.47
+1    3.47
+2    2.68
+3    3.19
+4    3.19
+Name: bore, dtype: object
+'''
 
 
 
